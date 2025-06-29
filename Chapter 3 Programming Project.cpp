33d0a8d06543bfb52ac366dff2164777615b17ff
PrttyPnkCoder <ddc43460@email.vccs.edu>
2025-06-29 18:51:07 -0400

    Added comments describing the programs to the master branch.

--- Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project.cpp
@@ -1,20 +1,3 @@
-// Chapter 3 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+// Description: A single C++ suite featuring a cookie?recipe scaler, a math tutor that pauses before revealing sums, and a compound?interest calculator to practice arithmetic.
+// Author: Demetrius Carter
+// Date: 6/29/2025
