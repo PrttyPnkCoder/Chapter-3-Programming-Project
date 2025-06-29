6077eb033554f40020b30bd650981c0fbb28722f
PrttyPnkCoder <ddc43460@email.vccs.edu>
2025-06-29 18:33:24 -0400

    Wrote the math tutor program.

--- Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project.cpp
@@ -1,20 +1,30 @@
-// Chapter 3 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// Presents a simple addition problem with two random numbers and shows the answer when Enter is pressed.
 #include <iostream>
+#include <cstdlib>      // for rand(), srand()
+#include <ctime>        // for time()
+#include <limits>      // for numeric_limits
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+    // Seed the random-number generator
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    // Generate two random numbers from 1 to 1000
+    int a = rand() % 1000 + 1;
+    int b = rand() % 1000 + 1;
+
+    // Present the problem
+    cout << "-- Math Tutor --\n";
+    cout << "Solve: " << a << " + " << b << " = ?\n";
+    cout << "(Press Enter when ready to see the answer)";
+
+    // Wait for the user to press Enter
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.get();
+
+    // Show the solution
+    cout << "Answer: " << (a + b) << "\n";
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
